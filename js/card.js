263f1351dbdeecc315c4b84ded870c19d1b3a5fd
Timur Kalimullin <timur_kalimullin@list.ru>
2020-11-05 14:01:54 +0500

    fix: fix translations

--- js/card.js
+++ js/card.js
@@ -1,7 +1,9 @@
 export default class Card {
   constructor(data) {
+    console.log(data)
     this.word = data.word;
     this.transcription = data.transcription;
+    this.translation = data.wordTranslate;
     this.image = `https://raw.githubusercontent.com/timurkalimullin/rslang/rslang-data/data/${data.image}`;
     this.audio = `https://raw.githubusercontent.com/timurkalimullin/rslang/rslang-data/data/${data.audio}`;
   }
@@ -13,20 +15,9 @@ export default class Card {
     <img src="./assets/volume.svg" alt="audio-image">
     <p class="word">${this.word}</p>
     <p class="transcription">${this.transcription}</p>
-    <p class="translation"></p>
     `;
-    let translation;
-    async function getTranslation (word) {
-      const url = `https://translate.yandex.net/api/v1.5/tr.json/translate?key=trnsl.1.1.20200423T141800Z.a13d7b3458025d70.13846b8c77630bf3379bbfeeb09d689a22a6605d&text= ${word} &lang=en-ru`;
-      const res = await fetch(url);
-      const data = await res.json();
-      const dataStr = await JSON.stringify(data);
-      card.querySelector('.translation').innerText = `${JSON.parse(dataStr).text}`;
-      translation = `${JSON.parse(dataStr).text}`;
-    }
-    getTranslation(this.word);
 
-    card.addEventListener('click', (event) => {
+    card.addEventListener('click', () => {
       if (!window.vars.play) {
         document.querySelectorAll('.card').forEach((el) => {
           el.classList.remove('active');
@@ -34,7 +25,7 @@ export default class Card {
         card.classList.add('active');
         new Audio(this.audio).play();
         document.querySelector('#image__wrapper').innerHTML = `<img src="${this.image}" alt="">`;
-        document.querySelector('#translation').innerHTML = `${translation}`;
+        document.querySelector('#translation').innerHTML = `${this.translation}`;
       }
     });
     return card;
